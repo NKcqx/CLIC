d59a9ac47101587228b2f996c908e8cc3ce146e9
zxp <starpo7@126.com>
2020-06-22 22:27:55 +0800

    In API 添加JSON parse

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -1,61 +1,70 @@
 import api.DataQuanta;
+import api.FrontJsonParse;
 import api.PlanBuilder;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.util.HashMap;
 
 
 public class demo {
-    public static void main(String[] args){
-        try {
-
-            PlanBuilder planBuilder = new PlanBuilder();
-            // 创建节点
-            DataQuanta source_node = planBuilder.readDataFrom(new HashMap<String, String>(){{
-                put("data_path", "source data file path");
-            }});
-            DataQuanta sort_node = DataQuanta.createInstance("sort", new HashMap<String, String>(){{
-                put("is_reverse", "false");
-                put("sorted_value", "sort sorted_value file path");
-                put("sorted_index", "sort sorted_index file path");
-            }});
-            DataQuanta filter_node = DataQuanta.createInstance("filter", new HashMap<String, String>(){{
-                put("predicate", "pre-defined or user-defined function");
-                put("result", "filter result file path");
-            }});
-            DataQuanta map_node = DataQuanta.createInstance("map", new HashMap<String, String>(){{
-                put("udf", "pre-defined or user-defined function");
-                put("result", "map result file path");
-            }});
-            DataQuanta collect_node = DataQuanta.createInstance("collect", new HashMap<String, String>(){{
-                put("result", "collect result file path");
-            }});
-
-            // 链接节点，即构建DAG
-            source_node.outgoing(sort_node, null);
-
-            //test
-            source_node.outgoing(collect_node,null);
-            //test
-            source_node.outgoing(filter_node,null);
-
-            sort_node.outgoing(map_node, new HashMap<String, String>(){{
-                put("sorted_value", "data");
-            }});
-            sort_node.outgoing(filter_node, new HashMap<String, String>(){{
-                put("sorted_value", "data");
-            }});
-
-          /*  collect_node.incoming(map_node, new HashMap<String, String>(){{
-                put("result", "data");
-            }});
-            collect_node.incoming(filter_node, new HashMap<String, String>(){{
-                put("result", "data");
-            }});*/
-            filter_node.outgoing(map_node,null);
-            planBuilder.execute();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+    public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
+//        try {
+//
+//            PlanBuilder planBuilder = new PlanBuilder();
+//            // 创建节点
+//            DataQuanta source_node = planBuilder.readDataFrom(new HashMap<String, String>(){{
+//                put("data_path", "source data file path");
+//            }});
+//            DataQuanta sort_node = DataQuanta.createInstance("sort", new HashMap<String, String>(){{
+//                put("is_reverse", "false");
+//                put("sorted_value", "sort sorted_value file path");
+//                put("sorted_index", "sort sorted_index file path");
+//            }});
+//            DataQuanta filter_node = DataQuanta.createInstance("filter", new HashMap<String, String>(){{
+//                put("predicate", "pre-defined or user-defined function");
+//                put("result", "filter result file path");
+//            }});
+//            DataQuanta map_node = DataQuanta.createInstance("map", new HashMap<String, String>(){{
+//                put("udf", "pre-defined or user-defined function");
+//                put("result", "map result file path");
+//            }});
+//            DataQuanta collect_node = DataQuanta.createInstance("collect", new HashMap<String, String>(){{
+//                put("result", "collect result file path");
+//            }});
+//
+//            // 链接节点，即构建DAG
+//            source_node.outgoing(sort_node, null);
+//
+//            //test
+//            source_node.outgoing(collect_node,null);
+//            //test
+//            source_node.outgoing(filter_node,null);
+//
+//            sort_node.outgoing(map_node, new HashMap<String, String>(){{
+//                put("sorted_value", "data");
+//            }});
+//            sort_node.outgoing(filter_node, new HashMap<String, String>(){{
+//                put("sorted_value", "data");
+//            }});
+//
+//          /*  collect_node.incoming(map_node, new HashMap<String, String>(){{
+//                put("result", "data");
+//            }});
+//            collect_node.incoming(filter_node, new HashMap<String, String>(){{
+//                put("result", "data");
+//            }});*/
+//            filter_node.outgoing(map_node,null);
+//            planBuilder.execute();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        PlanBuilder planBuilder = new PlanBuilder();
+        FrontJsonParse frontJsonParse = new FrontJsonParse();
+        frontJsonParse.parseJson("/Users/zhuxingpo/Downloads/ha.json");
+        frontJsonParse.connectDAG();
+        HashMap<Integer,DataQuanta> nodeList = frontJsonParse.getNodeOrderDataQuanta();
+        planBuilder.setHeadDataQuanta(nodeList.get(1));
     }
 }
