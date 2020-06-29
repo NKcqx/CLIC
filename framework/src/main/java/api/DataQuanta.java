7273161d03a2c3892191dc4b7cdd2a1909e9805a
NKcqx <892670992@qq.com>
2020-06-29 12:00:32 +0800

    Sub module (#21)
    * reorgnize_to_sub_module
    
    * reorgnize project to sub module

---
+++ framework/src/main/java/api/DataQuanta.java
@@ -0,0 +1,104 @@
+package api;
+
+import basic.Operators.Operator;
+import basic.Operators.OperatorFactory;
+import channel.Channel;
+
+import java.util.Map;
+
+/**
+ * 相当于DAG图中的每个节点，节点内部保存实际的Operator，提供一系列API以供构造下一跳（可以有多个）
+ */
+public class DataQuanta {
+    private Operator operator;
+
+    private DataQuanta(Operator operator){
+        this.operator = operator;
+    }
+
+    /**
+     * 根据ability创建一个DataQuanta，并载入（无依赖）参数值
+     * @param ability Operator要具有的功能
+     * @param params Operator参数的值，K-V形式，可为空；注意，这传入的参数值只能是静态的值，例如最大迭代次数、是否倒序，而不是依赖上一跳的输出
+     * @return 完整的DataQuanta
+     * @throws Exception 一系列错误的结合，包括XML结构解析错误、文件找不到、传入的key和配置文件里的参数名对不上等
+     */
+    public static DataQuanta createInstance(String ability, Map<String, String> params) throws Exception {
+        if (ability.equals("empty")){
+            return null; // TODO
+        }else {
+            // 先创建出符合要求的operator
+            Operator opt = DataQuanta.createOperator(ability);
+            if (params != null){
+                // 再设置静态的输入数据
+                for (Map.Entry entry : params.entrySet()){
+                    String key = (String) entry.getKey();
+                    String value = (String) entry.getValue();
+                    opt.setData(key, value);
+                }
+            }
+            DataQuanta dq = new DataQuanta(opt);
+            return dq;
+        }
+    }
+
+    /**
+     * 给this的opt添加一个新的上一跳opt
+     * @param incoming 上一跳opt
+     * @param params_pair 指定和上一跳Opt的输出的哪个数据的key链接，格式为 <incoming.output_key, this.input_key>；为null时默认拿到其所有的输出
+     * @return 当前已链接的incoming channel的数量，即代表有多少个上一跳了
+     */
+    public int incoming(DataQuanta incoming, Map<String, String> params_pair){
+        assert incoming != null : "上一跳不能为空";
+        Channel channel = new Channel(
+                incoming.getOperator(),
+                this.getOperator(),
+                params_pair
+        );
+        // 双向绑定
+        incoming.operator.connectTo(channel);
+        int num_incoming = this.operator.connectFrom(channel); // 返回下一个input_idx，不知道能干什么用
+        return num_incoming;
+    }
+
+    /**
+     * 给this的opt添加新的输出opt
+     * @param outgoing 下一跳opt
+     * @param params_pair 指定和下一跳Opt所需输入的哪个数据的key链接，格式为 <this.output_key, outgoing.input_key>；为null时默认传出所有数据
+     * @return 当前已链接的outgoing channel的数量，即代表有多少个下一跳了
+     */
+    public int outgoing(DataQuanta outgoing, Map<String, String> params_pair){
+        assert outgoing != null : "下一跳不能为空";
+        Channel channel = new Channel(
+                this.getOperator(),
+                outgoing.getOperator(),
+                params_pair
+        );
+        // 双向绑定
+        outgoing.operator.connectFrom(channel);
+        int num_incoming = this.operator.connectTo(channel); // 返回下一个input_idx，不知道能干什么用
+        return num_incoming;
+    }
+
+    /**
+     * 拿到DataQuanta所代表的Operator
+     * @return DataQuanta所代表的Operator
+     */
+    public Operator getOperator() {
+        return operator;
+    }
+
+    /**
+     * 功能单一，只是为了确保只有这一个地方提到了OperatorMapping的事
+     * @param operator_ability opt应该具有的功能
+     * @return 包含该opt的新的DataQuanta
+     * @throws Exception
+     */
+    private static Operator createOperator(String operator_ability) throws Exception {
+        // 先根据功能创建对应的opt
+        Operator opt = OperatorFactory.createOperator(operator_ability);
+        // 封装为DataQuanta
+        return opt;
+    }
+
+}
---
+++ framework/src/main/java/channel/Channel.java
@@ -0,0 +1,52 @@
+package channel;
+
+import basic.Operators.Operator;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @description：为{@link Operator}管理数据的存储位置、IO等工作
+ */
+public class Channel {
+
+    private Operator source_operator; // 边的起始点
+    private Operator target_operator; // 边的终点
+
+    private Map<String, String> key_pair; // source输出数据的key - target输入数据的key, 即source输出的值 传给 target输入数据的值
+
+    public Channel(Operator source, Operator target, Map<String, String> key_pair){
+        this.source_operator = source;
+        this.target_operator = target;
+        this.key_pair = key_pair;
+    }
+
+    public Channel(Operator source, List<String> output_keyset, Operator target, List<String> input_keyset) {
+        source_operator = source;
+        target_operator = target;
+    }
+
+    public Map<String, String> getKeyPair(){return this.key_pair;}
+
+//    /**
+//     * 从起点Opt把key代表的数据发送到终点Opt的指定Key中
+//     */
+//    public void transferData(){
+//        for (Map.Entry entry : this.key_pair.entrySet()){
+//            String source_key = (String) entry.getKey();
+//            String target_key = (String) entry.getValue();
+//            String source_value = source_operator.getOutputData(source_key);
+//            target_operator.setInputData(target_key, source_value);
+//        }
+//    }
+
+    public String retriveData(String key){
+        return source_operator.getOutputData(key);
+    }
+
+    public Operator getTargetOperator(){
+        return this.target_operator;
+    }
+    public Operator getSourceOperator(){return this.source_operator;}
+
+}
