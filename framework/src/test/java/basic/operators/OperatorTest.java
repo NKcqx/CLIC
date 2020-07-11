2d8f59dc4fef970572811d4dcb7c88e674c3a5ee
DLUTLiuFengyi <545184554@qq.com>
2020-07-11 11:50:28 +0800

    完成OperatorTest

--- framework/src/test/java/basic/operators/OperatorTest.java
+++ framework/src/test/java/basic/operators/OperatorTest.java
@@ -1,18 +1,18 @@
 package basic.operators;
 
+import basic.Param;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
@@ -24,13 +24,10 @@ import static org.mockito.Mockito.*;
  * @version 1.0
  * @since 2020/7/10 0:52
  */
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(Operator.class)
-@PowerMockIgnore({"javax.management.*"})
+@RunWith(PowerMockRunner.class) // 指定run的是PowerMockRunner
+@PrepareForTest(Operator.class) // 所有要测试的类（本例只有Operator.java）
+@PowerMockIgnore({"javax.management.*"}) // 解决使用Powermock之后的classloader错误
 public class OperatorTest {
-
-    @Mock
-    private Operator operator;
 
     @Before
     public void before() {
@@ -49,18 +46,20 @@ public class OperatorTest {
     @Test
     public void constructorTest() throws Exception {
         String configFilePath = "/framework/resources/Operator/Filter/conf/FilterOperator.xml";
-        PowerMockito.whenNew(Operator.class).withArguments(configFilePath).thenReturn(operator);
-        PowerMockito.doNothing().when(operator).loadOperatorConfs();
+        Operator spyOpt = PowerMockito.spy(
+                new Operator(configFilePath));
+        PowerMockito.whenNew(Operator.class).withArguments(configFilePath).thenReturn(spyOpt);
         Operator realOperator = new Operator(configFilePath);
         realOperator.loadOperatorConfs();
 
         // Both verifies had run
+        // realOperator已变成mock类型对象
         Mockito.verify(realOperator).loadOperatorConfs();
-        Mockito.verify(operator).loadOperatorConfs();
+        Mockito.verify(spyOpt).loadOperatorConfs();
     }
 
     @Test
-    // TODO
+    // TODO loadBasicInfo函数只在构造函数中调用，不方便进行Test
     public void loadBasicInfoTest() throws Exception {
         Operator opt = PowerMockito.spy(
                 new Operator("/framework/resources/Operator/Filter/conf/FilterOperator.xml"));
@@ -69,12 +68,33 @@ public class OperatorTest {
     }
 
     @Test
-    // TODO
+    // TODO loadParams函数只在构造函数中调用，不方便进行Test
     public void loadParamsTest() throws Exception {
         Operator spyOpt = PowerMockito.spy(
                 new Operator("/framework/resources/Operator/Filter/conf/FilterOperator.xml"));
         PowerMockito.doNothing().when(spyOpt, "loadParams");
         PowerMockito.verifyPrivate(spyOpt, Mockito.times(0)).invoke("loadParams");
+    }
+
+    @Test
+    public void inputDataListTest() throws Exception {
+        Operator opt = new Operator("/framework/resources/Operator/Filter/conf/FilterOperator.xml");
+        Operator spyOpt = Mockito.spy(opt);
+        String name = "udfName2";
+        String dataType = "string";
+        String defaultValue = null;
+        Boolean isRequired = false;
+        Param param = new Param(name, dataType, isRequired, defaultValue);
+        Map<String, Param> inputDataList = spyOpt.getInputDataList();
+        inputDataList.put(name, param);
+
+        // 使用Whitebox去访问私有成员
+        // 实际上getInputDataList()函数已让inputDataList可对外访问，Whitebox这行可以省略
+        Whitebox.setInternalState(spyOpt, "inputDataList", inputDataList);
+
+        for (Map.Entry<String, Param> entry : spyOpt.getInputDataList().entrySet()) {
+            // 打印出inputDataList可发现put函数调用成功
+        }
     }
 
     @Test
@@ -133,5 +153,45 @@ public class OperatorTest {
         boolean mockFlag = spyOpt.evaluate();
         assertEquals(false, mockFlag);
         verify(spyOpt, times(3)).evaluate();
+    }
+
+    @Test
+    public void tempPrepareDataTest() throws Exception {
+        Operator opt = new Operator("/framework/resources/Operator/Filter/conf/FilterOperator.xml");
+        Operator spyOpt = Mockito.spy(opt);
+        spyOpt.tempPrepareData();
+        verify(spyOpt, times(1)).tempPrepareData();
+    }
+
+    @Test
+    public void tempDoEvaluateTest() throws Exception {
+        Operator opt = new Operator("/framework/resources/Operator/Filter/conf/FilterOperator.xml");
+        Operator spyOpt = Mockito.spy(opt);
+        spyOpt.tempDoEvaluate();
+        verify(spyOpt, times(1)).tempDoEvaluate();
+    }
+
+    @Test
+    public void setDataAndSetInputDataTest() throws Exception {
+        Operator opt = new Operator("/framework/resources/Operator/Filter/conf/FilterOperator.xml");
+        Operator spyOpt = Mockito.spy(opt);
+
+        //spyOpt.setData("inputKey", "inputData"); // 此时会抛出NoSuchElementException“未在配置文件中...”
+
+        String name = "inputKey";
+        String dataType = "string";
+        String defaultValue = null;
+        Boolean isRequired = false;
+        Param param = new Param(name, dataType, isRequired, defaultValue);
+        Map<String, Param> inputDataList = spyOpt.getInputDataList();
+        inputDataList.put(name, param);
+
+        spyOpt.setData("inputKey", "inputData");
+
+        for (Map.Entry<String, Param> entry : spyOpt.getInputDataList().entrySet()) {
+            // 打印出inputDataList可发现setData函数和setInputData函数调用成功
+        }
+
+        verify(spyOpt, times(1)).setData("inputKey", "inputData");
     }
 }
