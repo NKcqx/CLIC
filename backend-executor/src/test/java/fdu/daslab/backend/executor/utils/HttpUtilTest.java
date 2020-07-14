4137782243d5f3d7eb4aca2bf2ce33de0087c47f
DLUTLiuFengyi <545184554@qq.com>
2020-07-14 16:36:07 +0800

    Revert "backend-executor单元测试初始化"
    This reverts commit 15686d8c0d68d323d88086a882c19beb20dc3dc8.

--- backend-executor/src/test/java/fdu/daslab/backend/executor/utils/HttpUtilTest.java
+++ backend-executor/src/test/java/fdu/daslab/backend/executor/utils/HttpUtilTest.java
@@ -1,34 +1,15 @@
 package fdu.daslab.backend.executor.utils;
 
-import junit.framework.TestCase;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.net.URL;
 
-public class HttpUtilTest extends TestCase {
+public class HttpUtilTest {
 
     @Test
     public void submitPipelineByYaml() {
         URL url = Thread.currentThread().getContextClassLoader().getResource("templates/argo-dag.yaml");
         assert url!=null;
         HttpUtil.submitPipelineByYaml(url.getPath());
-    }
-
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
-
-    @Test
-    public void postHttp() {
-    }
-
-    @Test
-    public void testSubmitPipelineByYaml() {
     }
 }
