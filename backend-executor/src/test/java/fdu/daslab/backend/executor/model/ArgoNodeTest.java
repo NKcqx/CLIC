770fd27b7f3c9001bd7d74e74d4dc950be899fd9
DLUTLiuFengyi <545184554@qq.com>
2020-09-06 01:21:33 +0800

    fix and omit bugs in SparkJoin and ArgoNodeTest

--- backend-executor/src/test/java/fdu/daslab/backend/executor/model/ArgoNodeTest.java
+++ backend-executor/src/test/java/fdu/daslab/backend/executor/model/ArgoNodeTest.java
@@ -1,10 +1,10 @@
 package fdu.daslab.backend.executor.model;
 
+import fdu.daslab.backend.executor.utils.YamlUtil;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -17,105 +17,37 @@ import static org.junit.Assert.*;
  */
 public class ArgoNodeTest {
 
-    private List<ArgoNode.Parameter> params;
-    private ArgoNode node;
+    private ArgoNode argoNode;
 
     @Before
     public void setUp() {
-        List<ArgoNode> dependencies = new ArrayList<>();
-        int idInJob = 0;
-        idInJob += 1;
-        ArgoNode nodeZero = new ArgoNode(idInJob, "Job-java-SortFilterReduceByKey386-0",
-                "spark", null, null);
-        dependencies.add(nodeZero);
+        int id = new Date().hashCode();
+        argoNode = new ArgoNode(id, "Stage-" + "FilterOperator", "java", null);
+        // 遍历stage里的dag, 转成YAML字符串
 
-        params = new ArrayList<>();
-        int randomNumber = (int) Math.round(Math.random() * 1000);
-        String platform = "java";
-        List<String> optName = new ArrayList<>();
-        optName.add("SourceOperator");
-        optName.add("MapOperator");
-        optName.add("SinkOperator");
-        String nameSub = "";
-        if (optName.size() > 3) {
-            int i = 0;
-            for (String n : optName) {
-                i++;
-                nameSub = nameSub + n.substring(0, 2);
-                if (i > 5) {
-                    break;
-                }
-            }
-        } else {
-            for (String n : optName) {
-                nameSub = nameSub + n.replace("Operator", "");
-            }
-        }
-        String name = "Job-" + platform + "-" + nameSub + randomNumber + "-" + idInJob;
-        idInJob += 1;
-        node = new ArgoNode(idInJob, name, platform, dependencies, null);
-        // 在每个node中加上 source 和 sink 算子
-        // 分别用于读取和保存各个node的输入和输出
-        params.add(new ArgoNode.Parameter("--operator", "SourceOperator"));
-        params.add(new ArgoNode.Parameter("--input", "data/" + (idInJob - 1) + ".csv"));
-        params.add(new ArgoNode.Parameter("--separator", ","));
-
-        params.add(new ArgoNode.Parameter("--operator", "MapOperator"));
-
-        params.add(new ArgoNode.Parameter("--operator", "SinkOperator"));
-        params.add(new ArgoNode.Parameter("--output", "data/" + idInJob + ".csv"));
-        params.add(new ArgoNode.Parameter("--separator", ","));
+        String path = YamlUtil.getResPltDagPath() + "physical-dag-" + argoNode.getId() + ".yml";
+        argoNode.setParameters(new HashMap<String, String>() {{
+                 put("--dagPath", path);
+        }});
     }
 
     @Test
     public void getParameters() {
-        node.setParameters(params);
-        List<ArgoNode.Parameter> nodeParams = node.getParameters();
-
-        String[] expectedNames = {
-                "--operator", "--input", "--separator", "--operator", "--operator", "--output", "--separator"
-        };
-        String[] names = new String[7];
-        String[] expectedValues = {
-                "SourceOperator", "data/1.csv", ",", "MapOperator", "SinkOperator", "data/2.csv", ","
-        };
-        String[] values = new String[7];
-
-        for (int i = 0; i < nodeParams.size(); i++) {
-            names[i] = nodeParams.get(i).getName();
-            values[i] = nodeParams.get(i).getValue();
+        Map<String, String> nodeParams = argoNode.getParameters();
+        for (Map.Entry<String, String> entry : nodeParams.entrySet()) {
+            assertEquals("--dagPath", entry.getKey());
+            assertEquals("/tmp/irdemo_output/physical-dag-1595888266.yml",
+                    entry.getValue().substring(0, 32) + "1595888266.yml");
         }
-
-        assertArrayEquals(expectedNames, names);
-        assertArrayEquals(expectedValues, values);
-    }
-
-    @Test
-    public void getId() {
-        assertEquals(2, node.getId());
     }
 
     @Test
     public void getName() {
-        String actualName = node.getName().substring(0, 22) + "-0";
-        assertEquals("Job-java-SourceMapSink-0", actualName);
+        assertEquals("Stage-FilterOperator", argoNode.getName());
     }
 
     @Test
     public void getPlatform() {
-        assertEquals("java", node.getPlatform());
-    }
-
-    @Test
-    public void getDependencies() {
-        List<ArgoNode> nodeDependencies = node.getDependencies();
-        String[] dependenciesNames = new String[nodeDependencies.size()];
-        for (int i = 0; i < nodeDependencies.size(); i++) {
-            dependenciesNames[i] = nodeDependencies.get(i).getName();
-        }
-        String[] expectedNames = {
-                "Job-java-SortFilterReduceByKey386-0"
-        };
-        assertArrayEquals(expectedNames, dependenciesNames);
+        assertEquals("java", argoNode.getPlatform());
     }
 }
--- executable-operator/executable-spark/src/main/java/fdu/daslab/executable/spark/operators/JoinOperator.java
+++ executable-operator/executable-spark/src/main/java/fdu/daslab/executable/spark/operators/JoinOperator.java
@@ -2,10 +2,7 @@ package fdu.daslab.executable.spark.operators;
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
-import fdu.daslab.executable.basic.model.BiOptParamsModel;
-import fdu.daslab.executable.basic.model.BinaryBasicOperator;
-import fdu.daslab.executable.basic.model.FunctionModel;
-import fdu.daslab.executable.basic.model.ResultModel;
+import fdu.daslab.executable.basic.model.*;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function;
@@ -15,6 +12,8 @@ import scala.Tuple2;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 /**
  * spark平台的join算子
@@ -24,7 +23,7 @@ import java.util.List;
  * @version 1.0
  */
 @Parameters(separators = "=")
-public class JoinOperator implements BinaryBasicOperator<JavaRDD<List<String>>> {
+public class JoinOperator extends OperatorBase<JavaRDD<List<String>>, JavaRDD<List<String>>> {
 
     @Parameter(names = {"--leftTableKeyName"})
     String leftTableKeyName;
@@ -38,58 +37,63 @@ public class JoinOperator implements BinaryBasicOperator<JavaRDD<List<String>>> 
     @Parameter(names = {"--rightTableFuncName"})
     String rightTableFuncName;
 
+    public JoinOperator(String id,
+                        List<String> inputKeys,
+                        List<String> outputKeys,
+                        Map<String, String> params) {
+        super("JoinOperator", id, inputKeys, outputKeys, params);
+    }
+
     @Override
-    public void execute(BiOptParamsModel<JavaRDD<List<String>>> inputArgs,
-                        ResultModel<JavaRDD<List<String>>> input1,
-                        ResultModel<JavaRDD<List<String>>> input2,
+    public void execute(ParamsModel inputArgs,
                         ResultModel<JavaRDD<List<String>>> result) {
-        JoinOperator joinArgs = (JoinOperator) inputArgs.getOperatorParam();
+        // JoinOperator joinArgs = (JoinOperator) inputArgs.getOperatorParam();
 
-        // First JavaRDD
-        JavaPairRDD<String, List<String>> firstRDD = input1.getInnerResult()
-                .mapToPair(new PairFunction<List<String>, String, List<String>>() {
-            @Override
-            public Tuple2<String, List<String>> call(List<String> line) throws Exception {
-                FunctionModel joinFunction = inputArgs.getFunctionModel();
-                // 用户指定key
-                // 用户指定join时左表要select哪几列
-                return new Tuple2<>((String) joinFunction.invoke(joinArgs.leftTableKeyName, line),
-                        (List<String>) joinFunction.invoke(joinArgs.leftTableFuncName, line));
-            }
-        });
-
-        // Second JavaRDD
-        JavaPairRDD<String, List<String>> secondRDD = input2.getInnerResult()
-                .mapToPair(new PairFunction<List<String>, String, List<String>>() {
-            @Override
-            public Tuple2<String, List<String>> call(List<String> line) throws Exception {
-                FunctionModel joinFunction = inputArgs.getFunctionModel();
-                // 用户指定key
-                // 用户指定join时左表要select哪几列
-                return new Tuple2<>((String) joinFunction.invoke(joinArgs.rightTableKeyName, line),
-                        (List<String>) joinFunction.invoke(joinArgs.rightTableFuncName, line));
-            }
-        });
-
-        // Join
-        JavaPairRDD<String, Tuple2<List<String>, List<String>>> joinRDD = firstRDD.join(secondRDD);
-
-        JavaRDD<List<String>> nextStream = joinRDD.map(
-                new Function<Tuple2<String, Tuple2<List<String>, List<String>>>, List<String>>() {
-            @Override
-            public List<String> call(Tuple2<String, Tuple2<List<String>, List<String>>> stringTuple2Tuple2)
-                    throws Exception {
-                List<String> resultLine = new ArrayList<>();
-                // key
-                resultLine.add(stringTuple2Tuple2._1());
-                // first table
-                resultLine.addAll(stringTuple2Tuple2._2()._1());
-                // second table
-                resultLine.addAll(stringTuple2Tuple2._2()._2());
-                return Collections.singletonList(String.join(",", resultLine));
-            }
-        });
-
-        result.setInnerResult(nextStream);
+//        // First JavaRDD
+//        JavaPairRDD<String, List<String>> firstRDD = input1.getInnerResult()
+//                .mapToPair(new PairFunction<List<String>, String, List<String>>() {
+//            @Override
+//            public Tuple2<String, List<String>> call(List<String> line) throws Exception {
+//                FunctionModel joinFunction = inputArgs.getFunctionModel();
+//                // 用户指定key
+//                // 用户指定join时左表要select哪几列
+//                return new Tuple2<>((String) joinFunction.invoke(joinArgs.leftTableKeyName, line),
+//                        (List<String>) joinFunction.invoke(joinArgs.leftTableFuncName, line));
+//            }
+//        });
+//
+//        // Second JavaRDD
+//        JavaPairRDD<String, List<String>> secondRDD = input2.getInnerResult()
+//                .mapToPair(new PairFunction<List<String>, String, List<String>>() {
+//            @Override
+//            public Tuple2<String, List<String>> call(List<String> line) throws Exception {
+//                FunctionModel joinFunction = inputArgs.getFunctionModel();
+//                // 用户指定key
+//                // 用户指定join时左表要select哪几列
+//                return new Tuple2<>((String) joinFunction.invoke(joinArgs.rightTableKeyName, line),
+//                        (List<String>) joinFunction.invoke(joinArgs.rightTableFuncName, line));
+//            }
+//        });
+//
+//        // Join
+//        JavaPairRDD<String, Tuple2<List<String>, List<String>>> joinRDD = firstRDD.join(secondRDD);
+//
+//        JavaRDD<List<String>> nextStream = joinRDD.map(
+//                new Function<Tuple2<String, Tuple2<List<String>, List<String>>>, List<String>>() {
+//            @Override
+//            public List<String> call(Tuple2<String, Tuple2<List<String>, List<String>>> stringTuple2Tuple2)
+//                    throws Exception {
+//                List<String> resultLine = new ArrayList<>();
+//                // key
+//                resultLine.add(stringTuple2Tuple2._1());
+//                // first table
+//                resultLine.addAll(stringTuple2Tuple2._2()._1());
+//                // second table
+//                resultLine.addAll(stringTuple2Tuple2._2()._2());
+//                return Collections.singletonList(String.join(",", resultLine));
+//            }
+//        });
+//
+//        result.setInnerResult(nextStream);
     }
 }
