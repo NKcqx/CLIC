cf4bf07f0ac4167de92f75b4d781180309d0bb44
Edward Tang <isedwardtang@gmail.com>
2021-03-18 15:48:03 +0800

    Update k8sinstall.sh

--- deployment/kubernetes/k8sinstall.sh
+++ deployment/kubernetes/k8sinstall.sh
@@ -8,14 +8,14 @@ sudo gpasswd -a ${USER} docker
 
 newgrp docker
 
-# 安装k8s
-cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+# 添加k8s的源
+vim /etc/apt/sources.list.d/kubernetes.list 
+# 添加一行
 deb http://mirrors.ustc.edu.cn/kubernetes/apt kubernetes-xenial main
-EOF
 
-apt-get update && apt-get install -y docker.io kubelet kubernetes-cni kubeadm
+sudo apt-get update && sudo apt-get install -y docker.io kubelet kubernetes-cni kubeadm
 
-# 替换key的后8位
+# 上述命令会报错，需要修改key，替换key为上面显示的key的后8位
 gpg --keyserver keyserver.ubuntu.com --recv-keys BA07F4FB
 gpg --export --armor BA07F4FB | sudo apt-key add -
 
@@ -23,13 +23,13 @@ kubeadm config images list
 
 # 从阿里云拉取镜像，这里的版本需要和上面保持一致
 images=(  # 下面的镜像应该去除"k8s.gcr.io/"的前缀，版本换成上面获取到的版本
-    kube-apiserver:v1.18.8
-    kube-controller-manager:v1.18.8
-    kube-scheduler:v1.18.8
-    kube-proxy:v1.18.8
+    kube-apiserver:v1.20.4
+    kube-controller-manager:v1.20.4
+    kube-scheduler:v1.20.4
+    kube-proxy:v1.20.4
     pause:3.2
-    etcd:3.4.3-0
-    coredns:1.6.7
+    etcd:3.4.13-0
+    coredns:1.7.0
 )
 
 for imageName in ${images[@]} ; do
