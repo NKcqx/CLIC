ac14835c89a9dcab80ebe23139bd7fc8764f1163
zjchenn <67681132+zjchenn@users.noreply.github.com>
2021-05-31 18:06:22 +0800

    注释了redis的测试

--- clic-master/src/test/java/fdu/daslab/redis/JedisUtilTest.java
+++ clic-master/src/test/java/fdu/daslab/redis/JedisUtilTest.java
@@ -1,30 +1,30 @@
-package fdu.daslab.redis;
-
-import fdu.daslab.scheduler.model.KubernetesStage;
-import org.junit.Test;
-import redis.clients.jedis.Jedis;
-
-
-/**
- * @author 唐志伟
- * @version 1.0
- * @since 2020/11/13 2:32 PM
- */
-public class JedisUtilTest {
-
+//package fdu.daslab.redis;
+//
+//import fdu.daslab.scheduler.model.KubernetesStage;
+//import org.junit.Test;
+//import redis.clients.jedis.Jedis;
+//
+//
+///**
+// * @author 唐志伟
+// * @version 1.0
+// * @since 2020/11/13 2:32 PM
+// */
+//public class JedisUtilTest {
+//
+////    @Test
+////    public void testConnect() {
+////        final Jedis jedis = JedisUtil.getClient();
+////        assert jedis != null;
+////        assert jedis.get("tzw").equals("handsome");
+////        jedis.close();
+////    }
+//
 //    @Test
-//    public void testConnect() {
-//        final Jedis jedis = JedisUtil.getClient();
-//        assert jedis != null;
-//        assert jedis.get("tzw").equals("handsome");
-//        jedis.close();
+//    public void testCreateStage(){
+//        KubernetesStage stage = new KubernetesStage("1710586742");
+//        StageRepository stageRepository = new StageRepository();
+//        stageRepository.addOrUpdateStage(stage);
 //    }
-
-    @Test
-    public void testCreateStage(){
-        KubernetesStage stage = new KubernetesStage("1710586742");
-        StageRepository stageRepository = new StageRepository();
-        stageRepository.addOrUpdateStage(stage);
-    }
-
-}
+//
+//}
